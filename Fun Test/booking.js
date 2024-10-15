5917ab10d53a5ae557660546d22d27df5de33184
Easin Arafat <arafatmrx@gmail.com>
2024-10-15 09:03:39 +0600

    box box

--- Fun Test/booking.js
+++ Fun Test/booking.js
@@ -11,7 +11,7 @@ require("chromedriver");
         until.elementLocated(
           By.css('button[aria-label="Dismiss sign-in info."]')
         ),
-        2000
+        5000
       );
       await dismissButton.click();
       console.log("Popup dismissed.");
@@ -22,6 +22,11 @@ require("chromedriver");
 
   try {
     await driver.get("https://www.booking.com/");
+
+    await driver.wait(
+      () => driver.executeScript("return document.readyState === 'complete';"),
+      10000
+    );
 
     await dismissPopupIfPresent();
 
@@ -40,8 +45,8 @@ require("chromedriver");
 
     await driver.wait(until.elementLocated(By.css("[data-date]")), 5000);
 
-    let checkInDate = moment().add(1, "days").format("YYYY-MM-DD");
-    let checkOutDate = moment().add(4, "days").format("YYYY-MM-DD");
+    let checkInDate = moment().format("YYYY-MM-DD");
+    let checkOutDate = moment().add(3, "days").format("YYYY-MM-DD");
 
     try {
       let checkInInput = await driver.wait(
@@ -74,29 +79,6 @@ require("chromedriver");
 
     await dismissPopupIfPresent();
 
-    try {
-      let sortByDropdown = await driver.wait(
-        until.elementLocated(
-          By.css('[data-testid="sorters-dropdown-trigger"]')
-        ),
-        5000
-      );
-      await sortByDropdown.click();
-
-      let distanceSortOption = await driver.wait(
-        until.elementLocated(
-          By.xpath("//span[contains(text(), 'Distance from closest beach')]")
-        ),
-        5000
-      );
-      await distanceSortOption.click();
-      console.log("Sorting by distance from closest beach.");
-    } catch (err) {
-      console.error("Error sorting by distance from the closest beach:", err);
-    }
-
-    await driver.sleep(3000);
-
     let hotels = await driver.findElements(
       By.css('[data-testid="property-card"]')
     );
@@ -126,5 +108,6 @@ require("chromedriver");
     }
   } finally {
     await driver.sleep(30000);
+    await driver.quit();
   }
 })();
